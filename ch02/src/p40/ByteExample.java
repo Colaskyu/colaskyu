c2cf3049f99fbe733b87ac6340988b09c322e9a8
bigdata <bigdata@user-PC>
2019-01-04 17:51:26 +0900

    2019.01.04 Java ch01~06 project register

---
+++ ch02/src/p40/ByteExample.java
@@ -0,0 +1,16 @@
+package p40;
+
+public class ByteExample {
+	
+	public static void main(String[] args) {
+		int a;
+		byte b;
+		a = 150;
+		
+		System.out.println("");
+		b = (byte)a;
+		System.out.println(b);
+		System.out.println(b & 0xff);
+		
+	}
+}
